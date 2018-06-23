ecba6cb8aa6878c991675850afcc3fab9cef28e7
enrikiko <enrikiko_91@hotmail.com>
2018-06-23 14:13:56 +0200

    SQL

---
+++ 9.Intermediate/SQL/WrittingClass.py
@@ -0,0 +1,25 @@
+import pymysql
+import datetime
+
+class User:
+    """This is a description of th class"""
+    def __init__(self, firstName, lastName, password, email):
+        self.firstName = firstName
+        self.lastName = lastName
+        self.password = password
+        self.email = email
+        self.time = datetime.datetime.now()
+    def save(self):
+        conn = pymysql.connect(host='localhost', user='root', passwd='root', db='pythondb', port=8889)
+        cursor = conn.cursor()
+        users_query = ("""INSERT INTO users(firstName, lastName, password, email, creationTime)  VALUES ('{}','{}','{}','{}','{}')""".format(self.firstName, self.lastName, self.password, self.email, self.time))
+        print(users_query)
+        cursor.execute(users_query)
+        conn.commit()
+    def hello(self):
+        print('Hello world this is {}'.format(self.firstName))
+
+
+
+Enrique = User('Enrique', 'Ramos', '123456', 'e@e.com')
+Enrique.save()
--- 9.Intermediate/SQL/creatingDatabase.py
+++ 9.Intermediate/SQL/creatingDatabase.py
@@ -1,6 +1,18 @@
 import pymysql
 conn = pymysql.connect(host='localhost', user='root', passwd='root', db='learning_over_here', port=8889)
 cursor = conn.cursor()
-users_query = ("CREATE DATABASE companies")
+users_query = ("SHOW DATABASES")
+cursor.execute(users_query)
+conn.commit()
+
+users_query = ("CREATE DATABASE pythondb")
+cursor.execute(users_query)
+conn.commit()
+
+users_query = ("USE pythondb")
+cursor.execute(users_query)
+conn.commit()
+
+users_query = ("CREATE TABLE users(firstName VARCHAR(50) NOT NULL,lastName VARCHAR(50) NOT NULL,password VARCHAR(16) NOT NULL,email VARCHAR(50) NOT NULL,creationTime VARCHAR(50) NOT NULL)")
 cursor.execute(users_query)
 conn.commit()
