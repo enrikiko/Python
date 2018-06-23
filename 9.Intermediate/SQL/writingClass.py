d82aa05e68521d724cf4860905a5a47fe54936c4
enrikiko <enrikiko_91@hotmail.com>
2018-06-23 14:17:31 +0200

    SQL

--- 9.Intermediate/SQL/writingClass.py
+++ 9.Intermediate/SQL/writingClass.py
@@ -1,6 +1,3 @@
-import pymysql
-import datetime
-
 class User:
     """This is a description of th class"""
     def __init__(self, firstName, lastName, password, email):
@@ -8,8 +5,10 @@ class User:
         self.lastName = lastName
         self.password = password
         self.email = email
+        import datetime
         self.time = datetime.datetime.now()
     def save(self):
+        import pymysql
         conn = pymysql.connect(host='localhost', user='root', passwd='root', db='pythondb', port=8889)
         cursor = conn.cursor()
         users_query = ("""INSERT INTO users(firstName, lastName, password, email, creationTime)  VALUES ('{}','{}','{}','{}','{}')""".format(self.firstName, self.lastName, self.password, self.email, self.time))
