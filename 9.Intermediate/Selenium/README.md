b07024199aa6d5042e6060e90ae607e419dc4307
enrikiko <enrikiko_91@hotmail.com>
2018-11-12 20:37:44 +0100

    sc

---
+++ 9.Intermediate/Selenium/README.md
@@ -0,0 +1,11 @@
+# Selenium
+
+## Install environment commands
+
+sudo apt install python-pip
+sudo pip install selenium
+
+
+> [Docker Tutorial](https://www.youtube.com/watch?v=tBfOHso1-6o&t=1s)
+> [Sintax Readme.md](https://help.github.com/articles/basic-writing-and-formatting-syntax)
+> [Git Commant](http://rogerdudler.github.io/git-guide/)
--- 9.Intermediate/Selenium/basic_selenium.py
+++ 9.Intermediate/Selenium/basic_selenium.py
@@ -4,7 +4,7 @@ from time import sleep
 url = 'https://www.linkedin.com/'
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-browser = webdriver.Chrome()
+browser = webdriver.Firefox(executable_path="./geckodriver")
 browser.set_window_position(0, 0)
 browser.set_window_size(1024, 768)
 browser.get(url)
@@ -14,7 +14,7 @@ sleep(1)
 input = browser.find_element_by_id('login-email')
 password = browser.find_element_by_id('login-password')
 input.send_keys('******@****.com')
-password.send_keys('··········')
+password.send_keys('1234')
 input.send_keys(Keys.ENTER)
 job_button = browser.find_element_by_id('jobs-tab-icon')
 job_button.click()
