6c58536f4c32b66abf99a80d8640caa74779c838
enrikiko <enrikiko_91@hotmail.com>
2019-05-12 22:08:18 +0200

    Auto

--- 9.Intermediate/Spyder/Artificial_Neural_Networks/init.py
+++ 9.Intermediate/Spyder/Artificial_Neural_Networks/init.py
@@ -17,31 +17,31 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('Churn_Modelling.csv')
+dataset = pd. read_csv('Churn_Modelling.csv')
 x = dataset.iloc[:, 3:13].values
 y = dataset.iloc[:, 13].values
 
 # Encoding categorical data
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 labelencoder_x_1 = LabelEncoder()
+labelencoder_x_2 = LabelEncoder()
 
 x[:, 1] = labelencoder_x_1.fit_transform(x[:, 1]) #convert Srtring to Number
-labelencoder_x_2 = LabelEncoder()
 x[:, 2] = labelencoder_x_2.fit_transform(x[:, 2])
 
-onehotencoder = OneHotEncoder(categorical_features = [1])
+onehotencoder = OneHotEncoder(categorical_features = [1], handle_unknown='ignore', dtype=np.integer) #encode dumy parameters
 x = onehotencoder.fit_transform(x).toarray()
 x = x[:, 1:]
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 0)
 
 # Feature Scaling
 from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
-X_train = sc.fit_transform(X_train)
-X_test = sc.transform(X_test)
+x_train = sc.fit_transform(x_train)
+x_test = sc.transform(x_test)
 
 # Part 2 - Now let's make the ANN!
 
@@ -67,26 +67,26 @@ classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'si
 classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
 
 # Fitting the ANN to the Training set
-classifier.fit(X_train, y_train, batch_size = 10, epochs = 100)
+classifier.fit(x_train, y_train, batch_size = 10, epochs = 100)
 
 # Part 3 - Making predictions and evaluating the model
 
 # Predicting the Test set results
-y_pred = classifier.predict(X_test)
-y_pred = (y_pred > 0.5)
+y_pred = classifier.predict(x_test)
+y_pred2 = (y_pred > 0.6)
 
 
 #Singl predictions
-newPredictions=classifier.predict(np.array([[0,0,600,1,40,3,6000,2,1,1,50000]]))
+newPredictions = classifier.predict(np.array([[0,0,600,1,40,3,6000,2,1,1,50000]]))
 print(newPredictions)
 # Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
 
 # ____________________________________________
-# | 1532 | 63 |
+# | 1532 | 63 |  1595
 # -------------
-# | 207 | 198 |
+# | 207 | 198 |  405
 # -------------
 # ( 1532 + 198 ) / 2000
 # % aciertos
