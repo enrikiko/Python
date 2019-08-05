8c59a6fe5123ec40fbc1214ded055352be155a06
enrikiko <enrikiko_91@hotmail.com>
2019-08-05 22:55:53 +0200

    Item right office simply

--- 9.Intermediate/Spyder/Convultional_Neural_Networks/src/api.py
+++ 9.Intermediate/Spyder/Convultional_Neural_Networks/src/api.py
@@ -5,17 +5,16 @@ app = Flask(__name__)
 @app.route('/example/')
 def example():
     return {'hello': 'world'}
-    
+
 @app.route('/upload')
 def upload_file():
    return render_template('upload.html')
 
-@app.route('/uploader', methods = ['GET', 'POST'])
+@app.route('/uploader')
 def upload_file():
-   if request.method == 'POST':
-      f = request.files['file']
-      f.save(secure_filename(f.filename))
-      return 'file uploaded successfully'
+  f = request.files['file']
+  f.save(secure_filename(f.filename))
+  return 'file uploaded successfully'
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0')
