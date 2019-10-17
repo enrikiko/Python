ba62cd7e17db463c3fd49971da113368da18d483
enrikiko <enrikiko_91@hotmail.com>
2019-10-17 22:51:51 +0200

    Carry federal body lawyer lawyer other

--- 9.Intermediate/Spyder/Recurrent_Neural_Networks/src/init.py
+++ 9.Intermediate/Spyder/Recurrent_Neural_Networks/src/init.py
@@ -4,12 +4,13 @@ import matplolib.pyplo as plt
 import pandas as pd
 
 dataset_trainning = pd.read_csv("Google_Stock_Price_Train.csv")
+trainning_set = dataset_trainning.iloc[:, 1:2].values
 
-app = Flask(__name__)
-
-@app.route('/example/')
-def example():
-    return {'hello': 'world'}
-
-if __name__ == "__main__":
-    app.run(debug=True,host='0.0.0.0')
+# app = Flask(__name__)
+#
+# @app.route('/example/')
+# def example():
+#     return {'hello': 'world'}
+#
+# if __name__ == "__main__":
+#     app.run(debug=True,host='0.0.0.0')
