81a50f38bfde998704e5cbe452cacb752dc35da2
enrikiko <enrikiko_91@hotmail.com>
2019-10-17 18:19:31 +0200

    do

--- 9.Intermediate/Spyder/Recurrent_Neural_Networks/src/init.py
+++ 9.Intermediate/Spyder/Recurrent_Neural_Networks/src/init.py
@@ -1,35 +1,15 @@
 from flask import Flask, render_template, request
-from werkzeug import secure_filename
-from
+import numpy as ny
+import matplolib.pyplo as plt
+import pandas as pd
+
+dataset_trainning = pd.read_csv("Google_Stock_Price_Train.csv")
+
 app = Flask(__name__)
 
 @app.route('/example/')
 def example():
     return {'hello': 'world'}
-
-@app.route('/upload')
-def upload_file():
-   return '''
-   <html>
-      <body>
-         <form action = "http://88.8.65.164:8030/uploader" method = "POST"
-            enctype = "multipart/form-data">
-            <input type = "file" name = "file" />
-            <input type = "submit"/>
-         </form>
-      </body>
-   </html>'''
-
- @app.route('/upload2')
- def upload_file():
-    return render_template('upload.html')
-
-
-@app.route('/uploader')
-def upload_file2():
-  f = request.files['file']
-  f.save(secure_filename(f.filename))
-  return 'file uploaded successfully'
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0')
