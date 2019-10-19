dd61b514e655691a2c6ae9d7e5fdb1a7a6797f05
enrikiko <enrikiko_91@hotmail.com>
2019-10-19 12:06:21 +0200

    Kitchen five while message

--- 9.Intermediate/Spyder/Recurrent_Neural_Networks/src/init.py
+++ 9.Intermediate/Spyder/Recurrent_Neural_Networks/src/init.py
@@ -1,10 +1,16 @@
 from flask import Flask, render_template, request
 import numpy as ny
-import matplolib.pyplo as plt
+#import matplotlib.pyplot as plt
+import matplotlib
 import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
 
+sc = MinMaxScaler(feature_range=(0, 1))
 dataset_trainning = pd.read_csv("Google_Stock_Price_Train.csv")
 trainning_set = dataset_trainning.iloc[:, 1:2].values
+print(trainning_set)
+trainning_set_scaled = sc.fit_transform(trainning_set)
+print(trainning_set_scaled)
 
 # app = Flask(__name__)
 #
