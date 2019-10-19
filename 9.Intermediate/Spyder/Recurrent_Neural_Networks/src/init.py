900f4813faefacbaaa3692cb5ae13fa360c86f0f
enrikiko <enrikiko_91@hotmail.com>
2019-10-19 12:52:52 +0200

    Firm discussion without operation hard teacher

--- 9.Intermediate/Spyder/Recurrent_Neural_Networks/src/init.py
+++ 9.Intermediate/Spyder/Recurrent_Neural_Networks/src/init.py
@@ -1,16 +1,28 @@
 from flask import Flask, render_template, request
-import numpy as ny
-#import matplotlib.pyplot as plt
+import numpy as np
+# import matplotlib.pyplot as plt
 import matplotlib
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 
 sc = MinMaxScaler(feature_range=(0, 1))
-dataset_trainning = pd.read_csv("Google_Stock_Price_Train.csv")
-trainning_set = dataset_trainning.iloc[:, 1:2].values
-print(trainning_set)
-trainning_set_scaled = sc.fit_transform(trainning_set)
-print(trainning_set_scaled)
+data_set_training = pd.read_csv("Google_Stock_Price_Train.csv")
+training_set = data_set_training.iloc[:, 1:2].values
+# print(training_set)
+training_set_scaled = sc.fit_transform(training_set)
+# print(training_set_scaled)
+x_train = []
+y_train = []
+for i in range(60, 1258):
+    x_train.append(training_set_scaled[i-60:i, 0])
+    y_train.append(training_set_scaled[i, 0])
+
+
+x_train, y_train = np.array(x_train), np.array(y_train)
+
+#Reshaping
+x_train = np.reshape(x_train, ())
+
 
 # app = Flask(__name__)
 #
