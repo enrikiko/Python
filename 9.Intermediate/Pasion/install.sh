b6857e3443e6506db8e4a6de98eb544047d7c194
enrikiko <enrikiko_91@hotmail.com>
2020-04-12 19:53:22 +0200

    Result accept might take

---
+++ 9.Intermediate/Pasion/install.sh
@@ -0,0 +1,4 @@
+sudo apt-get install python-pip &&
+	sudo apt install -y chromium-browser &&
+	sudo pip install selenium requests PyVirtualDisplay &&
+	echo "The enviroment is ready, run python index.py"
