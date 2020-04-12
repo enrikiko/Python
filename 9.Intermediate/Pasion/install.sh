6fa01fbf0ef635e4ab2a7380dfcc53299a8b77b7
enrikiko <enrikiko_91@hotmail.com>
2020-04-12 21:53:52 +0200

    Employee ask various also not

--- 9.Intermediate/Pasion/install.sh
+++ 9.Intermediate/Pasion/install.sh
@@ -1,4 +1,7 @@
 sudo apt-get install python-pip &&
-	sudo apt install -y chromium-browser &&
-	sudo pip install selenium requests PyVirtualDisplay &&
+	sudo apt remuve -y chromium-browser &&
+	sudo apt-get remuve python-selenium chromium-chromedriver &&
+	sudo apt autoremove &&
+	sudo apt-get remuve xvfb
+	sudo pip install selenium requests &&
 	echo "The enviroment is ready, run python index.py"
