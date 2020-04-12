459647ad6b7098edb9e4231ae5901261e7aa067a
enrikiko <enrikiko_91@hotmail.com>
2020-04-12 22:56:38 +0200

    Station light early hospital morning

--- 9.Intermediate/Pasion/browser.py
+++ 9.Intermediate/Pasion/browser.py
@@ -6,7 +6,8 @@ if platform.system() == "Darwin":
     browser = webdriver.Chrome('./chromedriverMac/chromedriver') #execute in Mac
 if platform.system() == 'Linux':
     chrome_options = Options()
-    chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--disable-dev-shm-usage')
+    #chrome_options.add_argument('headless')
+    #chrome_options.add_argument('--no-sandbox')
+    #chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_argument('window-size=1200x600')
     browser = webdriver.Chrome(chrome_options = chrome_options)#execute in Ubuntu
