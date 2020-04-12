99a2d288ef06fc8004e303186aad92ba0181c867
enrikiko <enrikiko_91@hotmail.com>
2020-04-12 19:51:50 +0200

    These because than foot hit road

--- 9.Intermediate/Pasion/browser.py
+++ 9.Intermediate/Pasion/browser.py
@@ -1,7 +1,10 @@
 from selenium import webdriver
+from pyvirtualdisplay import Display
 import platform
 
 if platform.system() == "Darwin":
     browser = webdriver.Chrome('./chromedriverMac/chromedriver') #execute in Mac
 if platform.system() == 'Linux':
+    display = Display(visible=0, size=(800, 800))
+    display.start()
     browser = webdriver.Chrome('./chromedriverUbuntu/chromedriver') #execute in Ubuntu
