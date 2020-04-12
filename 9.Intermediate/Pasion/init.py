2d6ecfdc143b7e67d1b836c447646f481caccefb
enrikiko <enrikiko_91@hotmail.com>
2020-04-12 19:47:38 +0200

    Cost pay even matter kill professor region pay

--- 9.Intermediate/Pasion/init.py
+++ 9.Intermediate/Pasion/init.py
@@ -1,9 +1,30 @@
-import os
-#from userInfo import jobTittle
+from private import url
+from private import context
+from browser import browser
+from pasionFunctions import enter
+from pasionFunctions import listAdvs
+from pasionFunctions import nextPage
+import platform
 
-#for job in jobTittle:
-    #try:
-os.system('python apply.py')
-    #except Exception as e:
-        #raise
+if __name__ == '__main__':
+    print("Web-auto-controlled.")
 
+
+currentPage = 1
+certain = True
+
+if platform.system() == "Darwin":
+    browser.set_window_position(0, 0)
+    browser.set_window_size(1280, 800)
+
+browser.get(url+context)
+print("Environment set")
+
+enter()
+while True:
+    listAdvs()
+    currentPage=nextPage(currentPage)
+
+
+browser.quit();
+print("Script end.")
