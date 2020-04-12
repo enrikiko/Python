33e90d205c9c4ed360ac9115c21a71ef5ca8dcc7
enrikiko <enrikiko_91@hotmail.com>
2020-04-12 23:01:09 +0200

    Four commercial assume term

--- 9.Intermediate/Pasion/browser.py
+++ 9.Intermediate/Pasion/browser.py
@@ -6,7 +6,7 @@ if platform.system() == "Darwin":
     browser = webdriver.Chrome('./chromedriverMac/chromedriver') #execute in Mac
 if platform.system() == 'Linux':
     chrome_options = Options()
-    chrome_options.add_argument('--headless')
+    #chrome_options.add_argument('--headless')
     #chrome_options.add_argument('--no-sandbox')
     #chrome_options.add_argument('--disable-dev-shm-usage')
     chrome_options.add_argument('window-size=1200x600')
