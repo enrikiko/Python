39295df0a879da1e71a59a46fc7889685790f0d1
enrikiko <enrikiko_91@hotmail.com>
2020-04-13 01:37:05 +0200

    Mother church few ground

--- 9.Intermediate/Pasion/browser.py
+++ 9.Intermediate/Pasion/browser.py
@@ -1,13 +1,13 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+#from selenium.webdriver.chrome.options import Options
 import platform
 
 if platform.system() == "Darwin":
     browser = webdriver.Chrome('./chromedriverMac/chromedriver') #execute in Mac
 if platform.system() == 'Linux':
-    chrome_options = Options()
+    #chrome_options = Options()
     #chrome_options.add_argument('--headless')
     #chrome_options.add_argument('--no-sandbox')
     #chrome_options.add_argument('--disable-dev-shm-usage')
-    chrome_options.add_argument('window-size=1200x600')
-    browser = webdriver.Chrome('/usr/lib/chromium-browser/chromedriver', chrome_options=chrome_options)#execute in Ubuntu
+    #chrome_options.add_argument('window-size=1200x600')
+    browser = webdriver.Firefox()#execute in Ubuntu
--- 9.Intermediate/Pasion/install.sh
+++ 9.Intermediate/Pasion/install.sh
@@ -1,6 +1,6 @@
 sudo apt-get install python-pip &&
 	sudo apt remove -y chromium-browser &&
-	sudo apt-get install python-selenium chromium-chromedriver &&
+	sudo apt-get remove -y python-selenium chromium-chromedriver &&
 	sudo apt autoremove &&
 	sudo apt-get remove xvfb
 	sudo pip install selenium requests &&
