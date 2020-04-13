4a2e9004615ebd66d3f55d6421e8b1b1e56e5f09
enrikiko <enrikiko_91@hotmail.com>
2020-04-13 23:04:00 +0200

    Particularly near early

--- 9.Intermediate/Pasion/install.sh
+++ 9.Intermediate/Pasion/install.sh
@@ -3,5 +3,5 @@ sudo apt-get install python-pip && \
 	sudo apt-get install -y python-selenium chromium-chromedriver && \
 	sudo apt autoremove && \
 	sudo pip install selenium requests pick ansible && \
-	ansible-vault decrypt vault_private.py && \
+	ansible-vault decrypt private.py && \
 	echo -e "\n\nThe enviroment is ready, run python index.py"
