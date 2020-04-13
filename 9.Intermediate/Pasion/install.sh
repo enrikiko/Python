0e591f3414dfda26ea145f01175428a49eaebec1
enrikiko <enrikiko_91@hotmail.com>
2020-04-13 02:42:23 +0200

    Off assume behavior money campaign current politics

--- 9.Intermediate/Pasion/install.sh
+++ 9.Intermediate/Pasion/install.sh
@@ -1,7 +1,7 @@
 sudo apt-get install python-pip &&
-	sudo apt remove -y chromium-browser &&
-	sudo apt-get remove -y python-selenium chromium-chromedriver &&
+	sudo apt install -y chromium-browser &&
+	sudo apt-get install -y python-selenium chromium-chromedriver &&
 	sudo apt autoremove &&
-	sudo apt-get remove xvfb
+	#sudo apt-get remove xvfb
 	sudo pip install selenium requests &&
 	echo "The enviroment is ready, run python index.py"
